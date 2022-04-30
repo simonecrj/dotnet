72f52127d55d43e7096e45f00d5192cc869a9b9f
simonecrj <33571775+simonecrj@users.noreply.github.com>
2022-04-30 15:59:05 -0300

    Aluno

---
+++ AlunoAltura.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String maMatricula;
+            Double maAltura;
+
+            String meMatricula;
+            Double meAltura;
+
+            String Matricula;
+            Double Altura;
+
+            Console.WriteLine("Determina o maior aluno e o menor aluno");
+            Console.Write("Informe a matricula do aluno: ");
+            Matricula = Console.ReadLine();
+            Console.Write("Informe a altura do aluno: ");
+            Altura = Convert.ToDouble(Console.ReadLine());
+
+            maAltura = Altura;
+            meAltura = Altura;
+            maMatricula = Matricula;
+            meMatricula = Matricula;
+            int i = 1;
+            while (i < 5)
+            {
+                Console.Write("Informe a matricula do aluno: ");
+                Matricula = Console.ReadLine();
+                Console.Write("Informe a altura do aluno: ");
+                Altura = Convert.ToDouble(Console.ReadLine());
+                if (maAltura < Altura)
+                {
+                    maMatricula = Matricula;
+                    maAltura = Altura;
+                }
+
+                if (meAltura > Altura)
+                {
+                    meMatricula = Matricula;
+                    meAltura = Altura;
+                }
+                i++;
+            }
+            Console.WriteLine("Maior aluno");
+            Console.WriteLine("Matricula: {0}, Altura: {1}", maMatricula, maAltura);
+            Console.WriteLine("Menor aluno");
+            Console.WriteLine("Matricula: {0}, Altura: {1}", meMatricula, meAltura);
+            Console.ReadKey();
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SalarioRea
+namespace ConsoleApp3
 {
     internal class Program
     {
@@ -28,6 +28,7 @@ namespace SalarioRea
                 Console.WriteLine("Salário reajustado: " + salarioNovo);
             }
             Console.ReadKey();
+
         }
     }
 }
